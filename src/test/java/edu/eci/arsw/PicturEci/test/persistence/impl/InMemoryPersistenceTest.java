8c9797e02bd425acc4eeda1d0a0166bad365a251
Daniel Rodriguez <daniel.rodriguez-tr@mail.escuelaing.edu.co>
2017-10-24 04:47:43 -0500

    test package added

---
+++ src/test/java/edu/eci/arsw/PicturEci/test/persistence/impl/InMemoryPersistenceTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.PicturEci.test.persistence.impl;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class InMemoryPersistenceTest {
+
+    
+}
