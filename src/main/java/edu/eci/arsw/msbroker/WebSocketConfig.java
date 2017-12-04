cc3bce1e44331b32e40841aa1305c24c999b5364
Daniel Rodriguez <daniel.rodriguez-tr@mail.escuelaing.edu.co>
2017-12-04 11:21:20 -0500

    rabbitMQ agregado

--- src/main/java/edu/eci/arsw/msbroker/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/msbroker/WebSocketConfig.java
@@ -9,6 +9,7 @@ package edu.eci.arsw.msbroker;
  *
  * @author 2098167
  */
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -19,9 +20,26 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    @Value("${host}")
+    private String host;
+    
+    @Value("${port}")
+    private int port;
+    
+    @Value("${clientLogin}")
+    private String clientLogin;
+    @Value("${clientPasscode}")
+    private String clientPasscode;
+    
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        //config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic/").setRelayHost(host).setRelayPort(port).
+                setClientLogin(clientLogin).
+                setClientPasscode(clientPasscode).
+                setSystemLogin(clientLogin).
+                setSystemPasscode(clientPasscode).
+                setVirtualHost(clientLogin);
         config.setApplicationDestinationPrefixes("/app");
     }
 
